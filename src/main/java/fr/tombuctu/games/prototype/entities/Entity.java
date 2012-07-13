6c6ca9f219e6626aacc28ba6592ca012a6942640
hjacquelin <hugo.jacquelin@gmail.com>
2012-07-14 01:45:47 +0200

    Added basic properties, getters and setters

--- src/main/java/fr/tombuctu/games/prototype/entities/Entity.java
+++ src/main/java/fr/tombuctu/games/prototype/entities/Entity.java
@@ -2,4 +2,32 @@ package fr.tombuctu.games.prototype.entities;
 
 public class Entity {
 
+	private String name;
+	private String id;
+
+	public Entity() {
+
+	}
+
+	public Entity(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
 }
